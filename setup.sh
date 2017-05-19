2439fdf8f9d3ef7f01cd678260e28515236f40c7
Natale Vinto <ebballon@gmail.com>
2017-05-19 12:38:31 +0200

    Fix typo

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ oc new-app https://raw.githubusercontent.com/openshift/origin/master/examples/je
 oc new-project development --display-name="Development" --description="Development project"
 oc new-app https://raw.githubusercontent.com/blues-man/rails-ex/master/openshift/templates/rails-postgresql-dev.json
 oc new-project testing --display-name="Testing" --description="Testing project"
-oc new-app https://raw.githubusercontent.com/blues-man/rails-ex/master/openshift/templates/rails-postgres-test.json
+oc new-app https://raw.githubusercontent.com/blues-man/rails-ex/master/openshift/templates/rails-postgresql-test.json
 oc project jenkins
 oc create -f https://raw.githubusercontent.com/blues-man/rails-ex/master/openshift/pipeline/development-pipeline.yaml
 oc create -f https://raw.githubusercontent.com/blues-man/rails-ex/master/openshift/pipeline/testing-pipeline.yaml
